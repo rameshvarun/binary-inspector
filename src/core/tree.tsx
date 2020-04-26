c376d08ae76fe6aad991e27b9de140c3ee670315
Varun Ramesh <varunramesh@fb.com>
2020-04-25 22:25:29 -0700

    Improve TreeView performance.

--- src/core/tree.tsx
+++ src/core/tree.tsx
@@ -6,6 +6,8 @@ export class Tree {
   children: Array<Tree>;
   error?: Error;
 
+  parent?: Tree;
+
   constructor(
     label: string,
     range: ByteRange | BitRange,
@@ -16,5 +18,24 @@ export class Tree {
     this.range = range;
     this.children = children;
     this.error = error;
+
+    // Set parent pointers for the children tree objects.
+    for (let child of this.children) {
+      child.parent = this;
+    }
+  }
+
+  isChildOf(other: Tree): boolean {
+    let current: Tree | undefined = this;
+
+    while (current !== undefined) {
+      if (current === other) return true;
+      current = current.parent;
+    }
+    return false;
+  }
+
+  isParentOf(other: Tree): boolean {
+    return other.isChildOf(this);
   }
 }
--- src/ui/treeview.tsx
+++ src/ui/treeview.tsx
@@ -3,20 +3,44 @@ import * as React from "react";
 import { ByteRange, BitRange } from "../core/range";
 import { Tree } from "../core/tree";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretRight, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
 const ERROR_BACKGROUND_COLOR = "#ff6e6e";
 
 export class TreeView extends React.Component<
   {
-    depth?: number;
+    depth: number;
     tree: Tree;
     selected?: Tree;
     onSelect?: (item: Tree) => void;
   },
-  {}
+  {
+    open: boolean;
+  }
 > {
+  public static defaultProps = {
+    depth: 0
+  };
+
   constructor(props) {
     super(props);
-    this.state = { selected: null };
+
+    this.state = { open: this.props.depth <= 1 };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.selected != nextProps.selected) {
+      let isBeingDeselected =
+        !!this.props.selected &&
+        this.props.tree.isParentOf(this.props.selected);
+      let isBeingSelected =
+        !!nextProps.selected && this.props.tree.isParentOf(nextProps.selected);
+
+      if (isBeingDeselected || isBeingSelected) return true;
+    }
+    if (this.state.open !== nextState.open) return true;
+    return false;
   }
 
   render() {
@@ -33,33 +57,46 @@ export class TreeView extends React.Component<
 
     if (tree.children.length > 0) {
       return (
-        <details
+        <div
           style={{
             wordWrap: "break-word",
             backgroundColor: backgroundColor,
             paddingLeft: "5px"
           }}
-          open={depth <= 1}
         >
-          <summary
+          <div
             onClick={() => {
               if (this.props.onSelect) this.props.onSelect(tree);
             }}
           >
-            {tree.label}
-          </summary>
+            <span
+              onClick={e => {
+                this.setState(state => {
+                  return { ...state, open: !state.open };
+                });
+                e.stopPropagation();
+              }}
+              style={{ paddingRight: "5px" }}
+            >
+              <FontAwesomeIcon
+                icon={this.state.open ? faCaretDown : faCaretRight}
+              />
+            </span>
+            <span>{tree.label}</span>
+          </div>
 
-          {tree.children.map((t, i) => (
-            <div key={i} style={{ paddingLeft: "15px" }}>
-              <TreeView
-                tree={t}
-                depth={depth + 1}
-                onSelect={this.props.onSelect}
-                selected={this.props.selected}
-              ></TreeView>
-            </div>
-          ))}
-        </details>
+          {this.state.open &&
+            tree.children.map((t, i) => (
+              <div key={i} style={{ paddingLeft: "15px" }}>
+                <TreeView
+                  tree={t}
+                  depth={depth + 1}
+                  onSelect={this.props.onSelect}
+                  selected={this.props.selected}
+                ></TreeView>
+              </div>
+            ))}
+        </div>
       );
     } else {
       return (
