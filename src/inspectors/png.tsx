6e5843ad6ff6c26c64c0aeb4b23dabc927e891d7
Varun Ramesh <varunramesh@fb.com>
2021-08-02 23:28:08 -0700

    Some more PNG chunk decoding code.

--- src/inspectors/png.tsx
+++ src/inspectors/png.tsx
@@ -9,6 +9,63 @@ import { Tree } from "../core/tree";
 
 import { hexEllipsis } from "../core/utils";
 
+type ChunkParser = {
+  name: string;
+  parser: (range: ByteRange) => Array<Tree>;
+};
+
+const CHUNK_PARSERS: Map<string, ChunkParser> = new Map();
+
+CHUNK_PARSERS.set("IHDR", {
+  name: "Image Header",
+  parser: (range: ByteRange) => {
+    let width = range.bytes(0, 4);
+    let height = range.bytes(4, 4);
+
+    let bitDepth = range.bytes(8, 1);
+    let colorType = range.bytes(9, 1);
+
+    let compressionMethod = range.bytes(10, 1);
+    let filterMethod = range.bytes(11, 1);
+    let interlaceMethod = range.bytes(12, 1);
+
+    return [
+      new Tree(`Width: ${width.readUIntBE()}`, width),
+      new Tree(`Height: ${height.readUIntBE()}`, height),
+
+      new Tree(`Bit Depth: ${bitDepth.readUIntBE()}`, bitDepth),
+
+      new Tree(`Color Type: ${colorType.readUIntBE()}`, colorType),
+
+      new Tree(
+        `Compression Method: ${compressionMethod.readUIntBE()}`,
+        compressionMethod
+      ),
+      new Tree(`Filter Method: ${filterMethod.readUIntBE()}`, filterMethod),
+      new Tree(
+        `Interlace Method: ${interlaceMethod.readUIntBE()}`,
+        interlaceMethod
+      )
+    ];
+  }
+});
+
+CHUNK_PARSERS.set("tEXt", {
+  name: "Textual Data",
+  parser: (range: ByteRange) => {
+    let nullIndex = range.toUint8Array().findIndex(val => val === 0);
+    let keyword = range.bytes(0, nullIndex);
+    let separator = range.bytes(nullIndex, 1);
+    let text = range.bytes(nullIndex + 1);
+
+    return [
+      new Tree(`Keyword: ${keyword.readUTF8()}`, keyword),
+      new Tree(`Null Separator`, separator),
+      new Tree(`Text: ${text.readUTF8()}`, text)
+    ];
+  }
+});
+
 function inspect(range: ByteRange): Tree {
   let signature = range.bytes(0, 8);
 
@@ -24,13 +81,22 @@ function inspect(range: ByteRange): Tree {
 
     let chunkSize = 8 + byteLen + 4;
 
+    let chunkParser = CHUNK_PARSERS.get(type.readUTF8());
+    let chunkElements = chunkParser
+      ? chunkParser.parser(data)
+      : [new Tree(`Data: ${hexEllipsis(data)}`, data)];
+
     chunks.push(
-      new Tree(`Chunk (${type.readUTF8()})`, ptr.bytes(0, chunkSize), [
-        new Tree(`Length: ${byteLen}`, length),
-        new Tree(`Type: ${type.readUTF8()}`, type),
-        new Tree(`Data: ${hexEllipsis(data)}`, data),
-        new Tree(`CRC: ${crc.readUIntBE()}`, crc)
-      ])
+      new Tree(
+        `Chunk (${type.readUTF8()})`,
+        ptr.bytes(0, chunkSize),
+        [
+          new Tree(`Length: ${byteLen}`, length),
+          new Tree(`Type: ${type.readUTF8()}`, type)
+        ]
+          .concat(chunkElements)
+          .concat([new Tree(`CRC: ${crc.readUIntBE()}`, crc)])
+      )
     );
 
     ptr = ptr.bytes(chunkSize);
