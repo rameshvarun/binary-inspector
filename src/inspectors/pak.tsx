130cef5330cb5f080d1330f2e3e99087d8b54802
Varun Ramesh <varunramesh@fb.com>
2021-08-02 23:42:48 -0700

    Quake PAK file improvements

--- src/inspectors/pak.tsx
+++ src/inspectors/pak.tsx
@@ -5,20 +5,43 @@ import { ByteRange, BitRange } from "../core/range";
 import { SimpleInspector } from "../ui/simpleinspector";
 import { Tree } from "../core/tree";
 
-function inspectFileEntry(range: ByteRange): Tree {
+function inspectFileEntry(
+  range: ByteRange,
+  file: ByteRange
+): { entry: Tree; contents: Tree } {
   let name = range.bytes(0, 56);
   let offset = range.bytes(56, 4);
   let size = range.bytes(60, 4);
 
-  return new Tree(`File Table Entry: ${name.readUTF8()}`, range, [
-    new Tree(`Name: ${name.readUTF8()}`, name),
-    new Tree(`File Offset: ${offset.readUIntLE()}`, offset),
-    new Tree(`File Size: ${size.readUIntLE()}`, size)
-  ]);
+  let contents = file.bytes(offset.readUIntLE(), size.readUIntLE());
+
+  return {
+    entry: new Tree(`File Table Entry: ${name.readUTF8()}`, range, [
+      new Tree(`Name: ${name.readUTF8()}`, name),
+      new Tree(`File Offset: ${offset.readUIntLE()}`, offset),
+      new Tree(`File Size: ${size.readUIntLE()}`, size)
+    ]),
+    contents: new Tree(`File Contents: ${name.readUTF8()}`, contents)
+  };
 }
 
-function inspectFileTable(range: ByteRange): Tree {
-  return new Tree(`File Table`, range, range.chunks(64).map(inspectFileEntry));
+function inspectFileTable(
+  range: ByteRange,
+  file: ByteRange
+): { table: Tree; contents: Tree } {
+  let files = range.chunks(64).map(entry => inspectFileEntry(entry, file));
+  return {
+    table: new Tree(
+      `File Table`,
+      range,
+      files.map(f => f.entry)
+    ),
+    contents: new Tree(
+      `File Contents`,
+      range,
+      files.map(f => f.contents)
+    )
+  };
 }
 
 function inspect(range: ByteRange): Tree {
@@ -27,12 +50,14 @@ function inspect(range: ByteRange): Tree {
   let tableSize = range.bytes(8, 4);
 
   let table = range.bytes(tableOffset.readUIntLE(), tableSize.readUIntLE());
+  let files = inspectFileTable(table, range);
 
   return new Tree("Quake PAK", range, [
     new Tree(`Magic: ${magic.readUTF8()}`, magic),
     new Tree(`File Table Offset: ${tableOffset.readUIntLE()}`, tableOffset),
     new Tree(`File Table Size: ${tableSize.readUIntLE()}`, tableSize),
-    inspectFileTable(table)
+    files.table,
+    files.contents
   ]);
 }
 
